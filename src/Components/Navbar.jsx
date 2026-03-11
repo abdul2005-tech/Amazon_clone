b17ede8fcc4ef0110b1fb365457df52364feb842
princeworks-ai <connectwithprincee@gmail.com>
2026-03-12 03:11:15 +0530

    Fix invalid CSS import in Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import logo from './logo.jpeg'
+import SideMenu from './SideMenu'
 import './sidemenu.css'
 import { useAuth, useCart } from '../context'
 
